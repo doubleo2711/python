22c1252c3d95570185c7f5afbb1c6153fff3ff2f
root <root@ip-172-31-24-148.ap-southeast-1.compute.internal>
2020-04-07 09:46:53 +0000

    Index of NgernTidLor version2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to Ngern Tid Lor"
+    return "Welcome to Ngern Tid Lor version2"
 
 @app.route('/how are you')
 def hello():
